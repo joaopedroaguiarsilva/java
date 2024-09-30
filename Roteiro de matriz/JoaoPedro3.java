5a7bc917beb9f9aa227ab261465a36725a505ae1
João Pedro Aguiar Silva <joaopedroaguiarsilva8@gmail.com>
2024-09-30 15:30:21 -0300

    exercicios

--- Roteiro de matriz/JoaoPedro3.java
+++ Roteiro de matriz/JoaoPedro3.java
@@ -4,12 +4,52 @@ public class JoaoPedro3 {
    Scanner teclado = new Scanner(System.in);
    
    
-   
+   int linhas, colunas;
 
+   System.out.println("Digite o número de linhas da sua segunda matriz: ");
+   linhas = teclado.nextInt();
+   System.out.println("Digte o número de colunas da sua segunda matriz: ");
+   colunas = teclado.nextInt();
+
+   double A[][]= new double[3][4];
+   double B[][]= new double[linhas][colunas];
+
+   preencherMatriz(A, teclado);
+   preencherMatriz(B, teclado);
+
+   if(linhas == 4)
+   {
+      double C[][]= new double[3][colunas];
+
+   }
+   else
+   {
+      System.out.println("Não é possível realizar uma multiplicação entre essas duas matrizes! ");
+   }
 
    teclado.close();
-
-
-   
    }
+
+   public static void preencherMatriz (double matriz [] [], Scanner teclado)
+   {
+      for(int i=0; i<matriz.length; i++) 
+      {
+         for(int j=0; j<matriz[0].length; j++) 
+         {
+            System.out.println("Digite o valor da linha "+(i+1) +" coluna "+(j+1)+":");
+            matriz[i][j] = teclado.nextDouble();
+         }
+      }    
+   }
+
+   public static void imprimeMatriz(double matriz[][]) {    	
+      for(int i=0; i<matriz.length; i++) 
+      {
+         for(int j=0; j<matriz[0].length; j++) 
+         {
+            System.out.print(matriz[i][j]+"\t");
+         }
+         System.out.println("");
+      }    
+   }  
 }
--- Roteiro de matriz/JoaoPedro5.java
+++ Roteiro de matriz/JoaoPedro5.java
@@ -3,13 +3,60 @@ public class JoaoPedro5 {
    public static void main(String []args){
    Scanner teclado = new Scanner(System.in);
    
-   
-   
+   double [] [] alunos_prova = new double [10][5];
 
+   String[] nomes = new String [10];
+   
+   preencherMatriz(alunos_prova, teclado);
+
+   for(int i =0; i < nomes.length; i++)
+   {
+      teclado.nextLine();
+      System.out.println("Digite o nome do "+ (i+1)+"* aluno: ");
+      nomes[i] = teclado.nextLine();
+   }
+
+   double soma [] = new double[10];
+   double media [] = new double [10];
+
+   for(int i= 0; i < alunos_prova.length; i++)
+   {
+      soma[i] = 0;
+      for(int j= 0; j < alunos_prova[0].length; j++)
+      {
+         soma[i] = soma[i] + alunos_prova[i][j];
+      }
+      media[i] = soma[i] /5;
+   }
+
+   for (int i = 0; i < alunos_prova.length; i++) 
+   {
+      if (media[i] >= 7) 
+      {
+         System.out.printf("Aluno %s foi aprovado com média de %.2f%n", nomes[i], media[i]);
+      } 
+      else if (media[i] >= 4 && media[i] < 7) 
+      {
+         System.out.printf("Aluno %s foi para o exame especial com média de %.2f%n", nomes[i], media[i]);
+      } 
+      else 
+      {
+         System.out.printf("Aluno %s foi reprovado com média de %.2f%n", nomes[i], media[i]);
+      }
+  }
 
    teclado.close();
-
-
+   }
    
+   public static void preencherMatriz (double matriz [] [], Scanner teclado)
+   {
+      for(int i=0; i<matriz.length; i++) 
+      {
+         for(int j=0; j<matriz[0].length; j++) 
+         {
+            System.out.println("Digite a nota do "+(i+1) +"* aluno na prova "+(j+1)+":");
+            matriz[i][j] = teclado.nextDouble();
+         }
+      }    
    }
 }
